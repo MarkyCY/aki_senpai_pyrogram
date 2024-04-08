9de320bbc407a0fdaa1b8101701c0a4a2d65cebd
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-07 21:45:23 -0400

    Change db async

--- plugins/commands/info.py
+++ plugins/commands/info.py
@@ -4,13 +4,13 @@ from pyrogram import filters
 
 from database.mongodb import get_db
 
-# Conectar a la base de datos
-db = get_db()
-users = db.users
-
 
 @Client.on_message(filters.command('info'))
 async def info_command(app: Client, message: Message):
+    # Conectar a la base de datos
+    db = await get_db()
+    users = db.users
+
     #Verificar si el mensaje es una respuesta a otro mensaje
     if message.reply_to_message is not None:
         #Si el mensaje es un reply a otro mensaje, obtengo los datos del usuario al que se le hizo reply
@@ -20,7 +20,7 @@ async def info_command(app: Client, message: Message):
             chat_member = await app.get_chat_member(message.chat.id, user.id)
             role_name = str(chat_member.status).split('.')[1]
             role = role_name.capitalize()
-            user_db = users.find_one({"user_id": user.id})
+            user_db = await users.find_one({"user_id": user.id})
             description = user_db.get('description', '-')
             #Envio la información del usuario y su rol en un mensaje de reply al mensaje original
             await app.send_message(message.chat.id, text=f"ID: {user.id}\nNombre: {user.first_name}\nNombre de usuario: @{user.username}\nRol: {role}\nDescripción: {description}", reply_parameters=ReplyParameters(message_id=message.reply_to_message_id))
@@ -38,7 +38,7 @@ async def info_command(app: Client, message: Message):
             chat_member = await app.get_chat_member(message.chat.id, user.id)
             role_name = str(chat_member.status).split('.')[1]
             role = role_name.capitalize()
-            user_db = users.find_one({"user_id": user.id})
+            user_db = await users.find_one({"user_id": user.id})
             description = user_db.get('description', '-')
             await app.send_message(message.chat.id, text=f"ID: {user.id}\nNombre: {user.first_name}\nNombre de usuario: @{user.username}\nRol: {role}\nDescripción: {description}", reply_parameters=ReplyParameters(message_id=message.id))
         except Exception as e:
