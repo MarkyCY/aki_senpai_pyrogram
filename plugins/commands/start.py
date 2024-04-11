9bf99202301646a0678a527b6f6bd0532561307a
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-10 23:02:28 -0400

    Fix chat type condition

--- plugins/commands/start.py
+++ plugins/commands/start.py
@@ -5,7 +5,8 @@ import asyncio
 
 @Client.on_message(filters.command('start'))
 async def start_command(app: Client, message: Message):
-    if (message.chat.type == 'private'):
+    chat_type = str(message.chat.type).split('.')[1].lower()
+    if chat_type == 'private':
         await app.send_message(message.chat.id, text="Hola para subscribirte en el concurso solo escribe o toca: /sub")
     else:
         await app.send_message(message.chat.id, text="Uhhh quieres participar? Contactame por PV y escribeme /sub \n@Akira_Senpai_bot")
