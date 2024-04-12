f2fc3b7642930d6b893157cc9eb1566898c3ce71
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-11 21:02:12 -0400

    Add functions to admins etc

---
+++ plugins/others/admin_func.py
@@ -0,0 +1,23 @@
+from database.mongodb import get_db
+
+async def isAdmin(user_id):
+    db = await get_db()
+    chat_admins = db.admins
+
+    isAdmin = None
+    admins = chat_admins.find()
+    async for admin in admins:
+        if admin['user_id'] == user_id:
+            isAdmin = "Yes"
+    return isAdmin
+
+async def isModerator(user_id):
+    db = await get_db()
+    users = db.users
+
+    isModerator = False
+    Users = users.find({"is_mod": True})
+    async for user in Users:
+        if user['user_id'] == user_id:
+            isModerator = True
+    return isModerator
