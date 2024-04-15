c1f2330f6ea0e1bcf4952ce6e4bf9339c3d539dc
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-15 17:14:27 -0400

    Add calendar function

--- API/YouTube/youtube_api.py
+++ API/YouTube/youtube_api.py
@@ -1,8 +1,12 @@
-import os
-import pickle
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
+
+import os
+import pytz
+import pickle
 import asyncio
+
+from datetime import datetime, timezone
 
 async def authenticate():
     scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
@@ -22,6 +26,28 @@ async def authenticate():
             pickle.dump(creds, token)
 
     return build("youtube", "v3", credentials=creds)
+
+async def authenticate_calendar():
+    # Definir los ámbitos de autorización necesarios para Google Calendar
+    scopes = ["https://www.googleapis.com/auth/calendar"]
+
+    # Cargar credenciales desde un archivo pickle si existe
+    if os.path.exists("API/YouTube/calendar_token.pickle"):
+        with open("API/YouTube/calendar_token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    else:
+        # Autenticar usando el archivo de credenciales de cliente
+        flow = InstalledAppFlow.from_client_secrets_file(
+            "API/YouTube/credentials.json", scopes=scopes
+        )
+        creds = await asyncio.get_event_loop().run_in_executor(None, flow.run_local_server)
+
+        # Guardar las credenciales en un archivo pickle para futuras ejecuciones
+        with open("API/YouTube/calendar_token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    # Construir el servicio de Google Calendar con las credenciales autenticadas
+    return build("calendar", "v3", credentials=creds)
 
 async def get_latest_videos(youtube, channel_id):
     videos_data = []
@@ -69,3 +95,46 @@ async def get_latest_videos(youtube, channel_id):
     videos_data.reverse()
 
     return videos_data
+
+async def list_calendars(service):
+    # Llamar al método list() del servicio de Google Calendar para obtener la lista de calendarios
+    calendar_list = await asyncio.get_event_loop().run_in_executor(None, service.calendarList().list().execute)
+
+    # Iterar sobre todos los calendarios y imprimir sus detalles
+    for calendar in calendar_list.get('items', []):
+        print('Id del calendario:', calendar['id'])
+        print('Nombre del calendario:', calendar['summary'])
+
+def convert_date(fecha_iso):
+    fecha_obj = datetime.fromisoformat(fecha_iso.replace('Z', '+00:00'))
+    zona_cuba = pytz.timezone('America/Havana')
+    fecha_cuba = fecha_obj.astimezone(zona_cuba)
+
+    fecha_formateada = fecha_cuba.strftime('%d/%m/%y %I:%M %p')
+
+    return fecha_formateada
+
+async def list_events(service):
+    now = datetime.now(tz=pytz.utc).isoformat()
+
+    event_list = await asyncio.get_event_loop().run_in_executor(
+        None,
+        service.events().list(
+            calendarId='5973845fcbada1fcf6c7fc498948df9091a15b37de9a014772641164c9e05db8@group.calendar.google.com',
+            timeMin=now,  # Establecer la fecha mínima para eventos a partir de ahora
+            maxResults=3,  # Limitar el número de eventos a 10
+            singleEvents=True,  # Expandir eventos recurrentes en eventos individuales
+            orderBy='startTime'  # Ordenar los eventos por su hora de inicio
+        ).execute
+    )
+    data = "<b>Próximos programas:</b>\n"
+    # Iterar sobre todos los calendarios y imprimir sus detalles
+    for event in event_list.get('items', []):
+        #print(event)
+        data += f"""
+<b>Título:</b> <code>{event['summary'].replace("OtkSen: ", "")}</code>
+<b>Descripción:</b> {event['description']}
+<b>Fecha:</b> <code>{convert_date(event['start']['dateTime'])}</code>
+"""
+
+    return data
