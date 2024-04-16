25d89aef987129361054629f6a2f2214d9ac2e2a
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-16 00:56:13 -0400

    Fix and clean

--- plugins/commands/programas.py
+++ plugins/commands/programas.py
@@ -4,12 +4,11 @@ from pyrogram import filters
 from pyrogram import enums
 
 from API.YouTube.youtube_api import authenticate_calendar, list_events
-from database.mongodb import get_db
 from plugins.others.contest import *
 
 
 @Client.on_message(filters.command('programas'))
-async def subs_command(app: Client, message: Message):
+async def programs_command(app: Client, message: Message):
     # Primero, autenticar y construir el servicio de Google Calendar
     service = await authenticate_calendar()
     # Luego, llamar a list_calendars() para obtener y mostrar la lista de calendarios
