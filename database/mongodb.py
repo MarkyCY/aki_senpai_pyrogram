e31cefb2885a9ec10a9b3e589ed1a2dbd83fe400
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-29 00:55:27 -0400

    Change db

--- database/mongodb.py
+++ database/mongodb.py
@@ -2,6 +2,5 @@ import os
 import motor.motor_asyncio
 
 async def get_db():
-    mongo_uri = os.getenv('MONGO_URI')
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
+    client = motor.motor_asyncio.AsyncIOMotorClient("127.0.0.1", 27017)
     return client.otakusenpai
