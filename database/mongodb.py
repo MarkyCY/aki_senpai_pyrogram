d8a9f2353a8e08f408ddea5bc271ef1a0632f759
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-04-29 02:13:41 -0400

    Test

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,10 +1,9 @@
 import os
 import motor.motor_asyncio
-from pymongo.server_api import ServerApi
 
 async def get_db():
     mongo_uri = os.getenv('MONGO_URI')
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri, server_api=ServerApi('1'))
+    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     try:
         await client.admin.command('ping')
         print("Pinged your deployment. You successfully connected to MongoDB!")
