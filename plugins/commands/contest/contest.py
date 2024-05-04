913436d4f279f12245a3e5d11220da6dbf6b3f64
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-05-04 02:27:30 -0400

    Add contest (sub modify)

---
+++ plugins/commands/contest/contest.py
@@ -0,0 +1,64 @@
+from pyrogram import Client
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import filters
+from pyrogram import enums
+
+from database.mongodb import get_db
+from plugins.others.contest import *
+
+import os
+
+@Client.on_message(filters.command('concursos'))
+async def contest_command(app: Client, message: Message, call_msg_id=None):
+    
+    chat_type = str(message.chat.type).split('.')[1].lower()
+    if not (chat_type == 'private'):
+        await message.reply_text(text="Este comando solo puede ser usado en privado.")
+        return
+
+    # Conectar a la base de datos
+    db = await get_db()
+    users = db.users
+    contest = db.contest
+
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+    username = message.from_user.username
+
+    chat_member = await app.get_chat_member(-1001485529816, user_id)
+
+    if chat_member is None:
+        await message.reply_text(text=f"Solo los integrantes de <a href='https://t.me/OtakuSenpai2020'>Otaku Senpai</a> pueden participar en el concurso.", parse_mode=enums.ParseMode.HTML)
+        return
+
+    if username is None:
+        await message.reply_text(text=f"Lo siento, no te puedes subscribir al concurso sin un nombre de usuario")
+        return
+    
+    user = users.find_one({'user_id': user_id})
+    if user:
+        pass
+    else:
+        await reg_user(user_id, username)
+
+    #contest_list = contest.find({'contest_num': 2})
+    contest_list = contest.find({'status': 'active'})
+    count = await contest_list.to_list(length=None)
+
+    if contest_list is None or len(count) == 0:
+        await app.send_message(chat_id, text=f"Lo siento, no hay ningún concurso abierto en este momento.")
+        return
+    
+    btns = []
+    list = [doc async for doc in contest.find({'status': 'active'})]
+    for contest in list:
+        title = contest['title']
+        btn = [InlineKeyboardButton(str(title), callback_data=f"show_contest_{int(contest['contest_num'])}")]
+        btns.append(btn)
+    markup = InlineKeyboardMarkup(inline_keyboard=btns)
+
+    if call_msg_id is not None:
+        await app.edit_message_text(chat_id, call_msg_id, text=f"Concursos Disponibles:", reply_markup=markup)
+        return
+    
+    await message.reply_text(text=f"Concursos Disponibles:", reply_markup=markup)
