3418e074000f22a1c93175b38a7b6f4b6e502066
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-10 12:14:36 -0400

    Donde Vote System

---
+++ plugins/callbacks/contest/vote_contest.py
@@ -0,0 +1,76 @@
+from pyrogram import Client
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram import filters, enums
+
+from database.mongodb import get_db
+from plugins.others.contest import *
+from bson import ObjectId
+
+import re
+
+@Client.on_callback_query(filters.regex(r"^vote_(10|[1-9])_\d{8,11}_[a-f\d]{24}$"))
+async def up_contest(app: Client, call: CallbackQuery):
+
+    db = await get_db()
+    contest = db.contest
+    Contest_Data = db.contest_data
+
+    JUECES = {938816655, 1881435398, 5602408597, 5825765407, 1221472021, 873919300, 5963355323}
+
+    uid = call.from_user.id
+    cid = call.message.chat.id
+    mid = call.message.id
+    
+    parts = call.data.split('_')
+    vote = int(parts[1])
+    u_vote = parts[2]
+    contest_data_id = ObjectId(parts[3])
+
+    emojis = {"1": "1️⃣","2": "2️⃣","3": "3️⃣","4": "4️⃣","5": "5️⃣","6": "6️⃣","7": "7️⃣","8": "8️⃣","9": "9️⃣","10": "🔟"}
+    if uid not in JUECES:
+        await app.answer_callback_query(call.id, f"Tu no eres un juez...")
+        return
+
+    
+    contest = await Contest_Data.find_one({'_id': contest_data_id})
+    if contest is None:
+        await app.answer_callback_query(call.id, f"No existe esta entrega en la base de datos...")
+        return
+         
+    if 'vote' in contest and str(uid) in contest['vote']:
+        await app.answer_callback_query(call.id, f"Votado actualizado de: {contest['vote'][str(uid)]} a {vote}")
+    else:
+        await app.answer_callback_query(call.id, f"Has Votado: {vote}")
+         
+    if contest['type'] == 'photo' or contest['type'] == 'video':
+        msg = call.message.caption
+    if contest['type'] == 'text':
+        msg = call.message.text
+
+    name = call.from_user.first_name
+    print(name)   
+
+    if re.search(r'(' + re.escape(name) + r'\d+️⃣|' + re.escape(name) + r'🔟)', msg):
+        msg = re.sub(r'(' + re.escape(name) + r'\d+️⃣|' + re.escape(name) + r'🔟)', f'{name}{emojis[str(vote)]}', msg)
+    else:
+        msg += f'\n{name}{emojis[str(vote)]}'
+    #msg = f"Foto de concurso:\nHas votado: {emojis[partes[2]]}"
+     
+    markup = InlineKeyboardMarkup(
+        [
+            [InlineKeyboardButton(str(i), callback_data=f"vote_{i}_{u_vote}_{contest_data_id}") for i in range(1, 6)],
+            [InlineKeyboardButton(str(i), callback_data=f"vote_{i}_{u_vote}_{contest_data_id}") for i in range(6, 12)]
+        ]
+    )
+     
+    if contest['type'] == 'photo':
+        await app.edit_message_caption(cid, mid, msg, reply_markup=markup)
+    if contest['type'] == 'text':
+        await app.edit_message_text(cid, mid, msg, reply_markup=markup)
+     
+    await Contest_Data.update_one(
+       { "_id": contest_data_id },
+       { "$set": { "vote." + str(uid): int(vote) } }
+    )
+         
+    return
