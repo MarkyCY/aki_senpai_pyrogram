4fc950f387f72b21279b2d7f62e1ab4679938747
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-13 22:13:42 -0400

    Add mute command

--- plugins/commands/admin/mute.py
+++ plugins/commands/admin/mute.py
@@ -1,89 +1,73 @@
-# from datetime import datetime, timedelta
-# from pyrogram import Client, filters
-# from pyrogram.types import ChatPermissions, Message
+from datetime import datetime, timedelta
+from pyrogram import Client, filters
+from pyrogram.types import ChatPermissions, Message
 
-# from database.mongodb import get_db
-# from plugins.others.admin_func import isModerator
+from database.mongodb import get_db
+from plugins.others.admin_func import isModerator
 
-# @Client.on_message(filters.command('aki_mute'))
-# async def mute_command(app: Client, message: Message):
-#     # Asegúrate de que el comando sea respondido a un mensaje
-#     if not message.reply_to_message:
-#         await message.reply("Por favor, responde al mensaje del usuario que deseas mutear.")
-#         return
+async def MuteUser(app, message, chat_id, user_id, name, until_date):
+    # Configurar los permisos de restricción (mutear al usuario)
+    permissions = ChatPermissions(can_send_messages=False)
 
-#     # ID del usuario a mutear
-#     user_mute = message.reply_to_message.from_user.id
+    # Calcular la fecha hasta la cual el usuario estará muteado (24 horas desde ahora)
+    until_date = datetime.now() + timedelta(days=1)
 
-#     # Obtener la ID del chat
-#     chat_id = message.chat.id
+    # Mutear al usuario
+    await app.restrict_chat_member(chat_id, user_id, permissions, until_date=until_date)
+    
+    await message.reply(f"El usuario {name} ha sido muteado por 24 horas.")
 
-#     # Verifica si el que ejecuta el comando es un moderador
-#     if not await isModerator(message.from_user.id):
-#         await message.reply("No tienes permisos para usar este comando.")
-#         return
+async def UnmuteUser(app, message, chat_id, user_id, name):
+    # Configurar los permisos de restricción (desmutear al usuario)
+    permissions = ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_polls=True, can_send_other_messages=True, can_add_web_page_previews=True)
 
-#     # Configurar los permisos de restricción (mutear al usuario)
-#     permissions = ChatPermissions(can_send_messages=False)
+    # Desmutear al usuario
+    await app.restrict_chat_member(chat_id, user_id, permissions)
 
-#     # Calcular la fecha hasta la cual el usuario estará muteado (24 horas desde ahora)
-#     until_date = datetime.now() + timedelta(days=1)
+    await message.reply(f"El usuario {name} ha sido desmuteado.")
 
-#     # Mutear al usuario
-#     await app.restrict_chat_member(chat_id, user_mute, permissions, until_date=until_date)
+@Client.on_message(filters.command('aki_mute'))
+async def mute_command(app: Client, message: Message):
+    # Asegúrate de que el comando sea respondido a un mensaje
+    if not message.reply_to_message:
+        await message.reply("Por favor, responde al mensaje del usuario que deseas mutear.")
+        return
 
-#     # Confirmar la acción en el chat
-#     await message.reply(f"El usuario {user_mute} ha sido muteado por 24 horas.")
+    # ID del usuario a mutear
+    user_mute_id = message.reply_to_message.from_user.id
+    name = message.reply_to_message.from_user.first_name
 
+    # Obtener la ID del chat
+    chat_id = message.chat.id
 
-# @Client.on_message(filters.command('aki_unmute'))
-# async def unmute_command(app: Client, message: Message):
-#     # Asegúrate de que el comando sea respondido a un mensaje
-#     if not message.reply_to_message:
-#         await message.reply("Por favor, responde al mensaje del usuario que deseas desmutear.")
-#         return
-
-#     # ID del usuario a desmutear
-#     user_unmute = message.reply_to_message.from_user.id
-
-#     # Obtener la ID del chat
-#     chat_id = message.chat.id
-
-#     # Verifica si el que ejecuta el comando es un moderador
-#     if not await isModerator(message.from_user.id):
-#         await message.reply("No tienes permisos para usar este comando.")
-#         return
-
-#     # Configurar los permisos de restricción (desmutear al usuario)
-#     permissions = ChatPermissions(can_send_messages=True, can_send_media_messages=True, can_send_polls=True, can_send_other_messages=True, can_add_web_page_previews=True)
-
-#     # Desmutear al usuario
-#     await app.restrict_chat_member(chat_id, user_unmute, permissions)
-
-#     # Confirmar la acción en el chat
-#     await message.reply(f"El usuario {user_unmute} ha sido desmuteado.")
+    chat_member = await app.get_chat_member(chat_id, user_mute_id)
+    role_name = str(chat_member.status).split('.')[1]
+    if not await isModerator(message.from_user.id) or role_name.lower() not in ['administrator', 'owner']:
+        await message.reply("No tienes permisos para usar este comando.")
+        return
+    
+    await MuteUser(app, message, chat_id, user_mute_id, name, until_date=None)
 
 
-# @Client.on_message(filters.command('aki_delete'))
-# async def delete_message_command(app: Client, message: Message):
-#     # Asegúrate de que el comando sea respondido a un mensaje
-#     if not message.reply_to_message:
-#         await message.reply("Por favor, responde al mensaje que deseas eliminar.")
-#         return
 
-#     # Obtener el ID del mensaje a eliminar
-#     message_id_to_delete = message.reply_to_message.id
+@Client.on_message(filters.command('aki_unmute'))
+async def unmute_command(app: Client, message: Message):
+    # Asegúrate de que el comando sea respondido a un mensaje
+    if not message.reply_to_message:
+        await message.reply("Por favor, responde al mensaje del usuario que deseas desmutear.")
+        return
 
-#     # Obtener la ID del chat
-#     chat_id = message.chat.id
+     # ID del usuario a mutear
+    user_mute_id = message.reply_to_message.from_user.id
+    name = message.reply_to_message.from_user.first_name
 
-#     # Verifica si el que ejecuta el comando es un moderador
-#     if not await isModerator(message.from_user.id):
-#         await message.reply("No tienes permisos para usar este comando.")
-#         return
+    # Obtener la ID del chat
+    chat_id = message.chat.id
 
-#     # Eliminar el mensaje
-#     await app.delete_messages(chat_id, message_id_to_delete)
-
-#     # Confirmar la acción en el chat
-#     await message.reply("El mensaje ha sido eliminado.")
+    chat_member = await app.get_chat_member(chat_id, user_mute_id)
+    role_name = str(chat_member.status).split('.')[1]
+    if not await isModerator(message.from_user.id) or role_name.lower() not in ['administrator', 'owner']:
+        await message.reply("No tienes permisos para usar este comando.")
+        return
+    
+    await UnmuteUser(app, message, chat_id, user_mute_id, name)
