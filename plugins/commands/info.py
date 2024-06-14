80096d6bfaff9b3ef513d7f69157e0426e76e017
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-14 00:10:25 -0400

    Better info aparence with buttons and emojis etc

--- plugins/commands/info.py
+++ plugins/commands/info.py
@@ -6,55 +6,57 @@ from database.mongodb import get_db
 
 
 @Client.on_message(filters.command('info'))
-async def info_command(app: Client, message: Message):
+async def info_command(app: Client, message: Message, user_data=None):
     # Conectar a la base de datos
     db = await get_db()
     users = db.users
 
+    if user_data is None:
+        user = message.reply_to_message.from_user
+        if message.reply_to_message is None:
+            await message.reply_text('Por favor, responde a un mensaje')
+            return
+    else:
+        user = user_data
+    
+    user_action_id = user.id
+    
     btns = [
         [
-            InlineKeyboardButton("⚠️Advertencias", callback_data=f"warn_{message.from_user.id}"),
-            InlineKeyboardButton("🪪Roles", callback_data=f"show_roles"),
+            InlineKeyboardButton("⚠️Advertencias", callback_data=f"warn_{user_action_id}"),
+            InlineKeyboardButton("🪪Roles", callback_data=f"show_roles_{user_action_id}"),
         ],
         [
-            InlineKeyboardButton("🤫Silenciar", callback_data=f"mute_{message.from_user.id}"),
-            InlineKeyboardButton("⛔️Banear", callback_data=f"ban_{message.from_user.id}")
+            InlineKeyboardButton("🔇Silenciar", callback_data=f"mute_{user_action_id}"),
+            InlineKeyboardButton("⛔️Banear", callback_data=f"ban_{user_action_id}")
         ]
     ]
     markup = InlineKeyboardMarkup(inline_keyboard=btns)
     
-    # Verificar si el mensaje es una respuesta a otro mensaje
-    if message.reply_to_message is not None:
-        message.reply_text('Por favor, responde a un mensaje')
-        return
-    
-    # Si el mensaje es un reply a otro mensaje, obtengo los datos del usuario al que se le hizo reply
-    user = message.reply_to_message.from_user
     # Obtengo el rol del usuario en el chat
-    try:
-        chat_member = await app.get_chat_member(message.chat.id, user.id)
-        role_name = str(chat_member.status).split('.')[1]
-        role = role_name.capitalize()
-     
+    chat_member = await app.get_chat_member(message.chat.id, user.id)
+    role_name = str(chat_member.status).split('.')[1]
+    role = role_name.capitalize()
+    
+    user_db = await users.find_one({"user_id": user.id})
+    
+    if user_db is None:
+        await users.insert_one({
+            "user_id": user.id
+        })
         user_db = await users.find_one({"user_id": user.id})
-     
-        if user_db is None:
-            await users.insert_one({
-                "user_id": user.id
-            })
-            user_db = await users.find_one({"user_id": user.id})
-     
-        description = user_db.get('description', '-')
-        # Envio la información del usuario y su rol en un mensaje de reply al mensaje original
-        await app.send_message(message.chat.id, text=f"""🆔ID: {user.id}
+    
+    description = user_db.get('description', '-')
+
+    msg=f"""🆔ID: {user.id}
 👱Nombre: {user.first_name}
 👤Nombre de usuario: @{user.username}
 🪪Rol: {role}
 ⚠️Advertencias: {user_db.get('warnings', 0)}/3
 ℹ️Descripción: {description}
-""", reply_parameters=ReplyParameters(message_id=message.reply_to_message_id), reply_markup=markup)
-    except Exception as e:
-        print(e)
-        chat_member = await app.get_users(user.id)
-        # Envio la información del usuario y su rol en un mensaje de reply al mensaje original
-        await app.send_message(message.chat.id, text=f"ID: {user.id}\nNombre: {user.first_name}\nNombre de usuario: @{user.username}", reply_parameters=ReplyParameters(message_id=message.reply_to_message_id))
+"""
+    
+    if user_data is None:
+        await app.send_message(message.chat.id, text=msg, reply_parameters=ReplyParameters(message_id=message.reply_to_message_id), reply_markup=markup)
+    else:
+        await app.edit_message_text(message.chat.id, message.id, text=msg, reply_markup=markup)
