e42465339e4ccd1de0efe86a02de55150164cd13
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-18 20:41:36 -0400

    Add function to calculate time

--- plugins/others/admin_func.py
+++ plugins/others/admin_func.py
@@ -1,4 +1,7 @@
 from database.mongodb import get_db
+import datetime
+from pyrogram import utils
+import re
 
 async def isAdmin(user_id):
     db = await get_db()
@@ -32,3 +35,21 @@ async def isCollaborator(user_id):
         if user['user_id'] == user_id:
             isCollaborator = True
     return isCollaborator
+
+def get_until_date(time):
+    match_duration = re.match(r'^(\d+)([dhm])$', time) if time else None
+    reason = time if match_duration else None
+    until_date = utils.zero_datetime()
+    if reason:
+        match = re.match(r'^(\d+)([dhm])$', reason)
+        if match:
+            num = int(match.group(1))
+            unit = match.group(2)
+            if unit == 'h':
+                until_date = datetime.datetime.now() + datetime.timedelta(hours=num)
+            elif unit == 'd':
+                until_date = datetime.datetime.now() + datetime.timedelta(days=num)
+            elif unit == 'm':
+                until_date = datetime.datetime.now() + datetime.timedelta(minutes=num)
+                
+    return until_date
