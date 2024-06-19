cf364c468ff8201e074f463e13ff4fb48bbac86e
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-06-18 20:41:19 -0400

    Change cmd to warn and fix tags

--- plugins/commands/admin/warn.py
+++ plugins/commands/admin/warn.py
@@ -1,7 +1,5 @@
-from datetime import datetime, timedelta
-from pyrogram import utils
 from pyrogram import Client, filters
-from pyrogram.types import ChatPermissions, Message
+from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 
 from database.mongodb import get_db
 from plugins.commands.admin.mute import MuteUser
@@ -56,15 +54,34 @@ async def remove_warning(user_id):
     else:
         return "Este usuario no tiene advertencias."
     
-@Client.on_message(filters.command('aki_warn'))
+@Client.on_message(filters.command('warn'))
 async def warn_command(app: Client, message: Message):
-    # Asegúrate de que el comando sea respondido a un mensaje
-    if not message.reply_to_message:
-        await message.reply("Por favor, responde al mensaje de un usuario.")
-        return
 
-    # ID del usuario a mutear
-    user_warn_id = message.reply_to_message.from_user.id
+    if message.command and len(message.command) > 1:
+        elemento = message.command[1]
+        
+        # Si el elemento es un ID de usuario
+        if elemento.isdigit() and 8 <= len(elemento) <= 11:
+            user_id = int(elemento)
+            get_user = await app.get_chat_member(message.chat.id, user_id)
+            user_warn_id = get_user.user.id
+            name = get_user.user.first_name
+            
+
+        # Si el elemento es un nombre de usuario
+        elif elemento.startswith('@'):
+                username = elemento.replace('@', '')
+                get_user = await app.get_chat_member(message.chat.id, username)
+                user_warn_id = get_user.user.id
+                name = get_user.user.first_name
+    
+    elif not message.reply_to_message:
+        await message.reply("Por favor, responde al mensaje del usuario que deseas mutear.")
+        return
+    
+    else:
+        user_warn_id = message.reply_to_message.from_user.id
+        name = message.reply_to_message.from_user.first_name
 
     # Obtener la ID del chat
     chat_id = message.chat.id
@@ -83,4 +100,11 @@ async def warn_command(app: Client, message: Message):
         return
     
     Warn = await add_warning(app, chat_id, user_warn_id)
-    await message.reply(Warn)
+    btns = [
+        [
+            InlineKeyboardButton(
+                "-1 Warn", callback_data=f"remove_warn_{user_warn_id}"),
+        ]
+    ]
+    markup = InlineKeyboardMarkup(inline_keyboard=btns)
+    await message.reply(Warn, reply_markup=markup)
