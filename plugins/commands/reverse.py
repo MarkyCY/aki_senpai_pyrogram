ee8571b596c36afa61ec5c34787a80738f777ece
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-07-18 22:24:08 -0400

    ahhh! problems

--- plugins/commands/reverse.py
+++ plugins/commands/reverse.py
@@ -25,13 +25,13 @@ async def reverse_command(app: Client, message: Message):
         await message.reply_text(text="Este comando es exclusivo de Otaku Senpai.")
         return
     
-    if not message.reply_to_message:
-        print("no reply!")
-        return
+    #if not message.reply_to_message:
+    #    print("no reply!")
+    #    return
     
-    if (message.reply_to_message.id != 1733263647 and message.reply_to_message.id !=1964681186):
-        print("no bot!")
-        return
+    #if (message.reply_to_message.id != 1733263647 and message.reply_to_message.id !=1964681186):
+    #    print("no bot!")
+    #    return
     
     if not message.reply_to_message.photo:
         await message.reply_text(text=f"Debes hacer reply a una imagen para poder describirla")
