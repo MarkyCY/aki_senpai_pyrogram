8d581991a6f10e9c1f8aa0778f5576c04b812f3d
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-09-07 00:05:12 -0400

    Fix

--- plugins/commands/reverse.py
+++ plugins/commands/reverse.py
@@ -65,12 +65,15 @@ async def reverse_command(app: Client, message: Message):
     downloaded_file = await app.download_media(message.reply_to_message, file_name="image.jpg", progress=progress)
 
     result = await async_post_image(url, params, downloaded_file)
-    print(result)
+    
     res = json.loads(result)
 
     #print(res)
 
-    await app.set_reaction(chat_id, message.id, reaction=[ReactionTypeEmoji(emoji="👨‍💻")])
+    try:	
+        await app.set_reaction(chat_id, message.id, reaction=[ReactionTypeEmoji(emoji="👨‍💻")])
+    except:
+        pass
 
     print(
         f"Haciendo soliticud a SauceNAO... user: @{message.from_user.username}")
