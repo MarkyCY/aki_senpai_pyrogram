206c5b7d3ea5727d9157f2e82d3d30a9b35cd388
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-09-10 00:33:21 -0400

    set kill command for security

--- plugins/commands/admin/restart.py
+++ plugins/commands/admin/restart.py
@@ -18,3 +18,18 @@ async def restart_command(app: Client, message: Message):
     await message.reply_text("Reiniciando...")
     # shutil.rmtree("downloads", ignore_errors=True)
     os.system(f"kill -9 {os.getpid()} && bash startup.sh")
+
+@Client.on_message(filters.command('kill'))
+async def kill_command(app: Client, message: Message):
+    chat_id = message.chat.id
+    user_id = message.from_user.id
+
+    if chat_id != -1001485529816:
+        return await message.reply_text("Este comando es exclusivo de Otaku Senpai")
+
+    if user_id != 873919300:
+        return await message.reply_text("No tienes permisos para usar este comando.")
+
+    await message.reply_text("Bye...")
+    # shutil.rmtree("downloads", ignore_errors=True)
+    os.system(f"kill -9 {os.getpid()}")
