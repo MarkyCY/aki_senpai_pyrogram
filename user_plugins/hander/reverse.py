dfda964d09e02e986df897aba9f7e4cf9824ea67
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-09 19:00:57 -0400

    Fix test

--- user_plugins/hander/reverse.py
+++ user_plugins/hander/reverse.py
@@ -1,9 +1,6 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-import asyncio
-
-
 @Client.on_message(filters.photo & filters.group & filters.bot & (filters.user(1733263647) | filters.user(1964681186)))
 async def reverse_handler(app: Client, message: Message):
     
@@ -12,4 +9,4 @@ async def reverse_handler(app: Client, message: Message):
     if text[1] != "waifu" and text[1] != "husbando":
         return
     
-    await message.reply_text("wa")
+    return await message.reply_text("wa")
