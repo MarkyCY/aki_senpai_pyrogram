992edf85b113455ada10f81dda6547fbc87db569
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-10-24 05:52:52 -0400

    Not async

--- user_plugins/hander/reverse.py
+++ user_plugins/hander/reverse.py
@@ -1,12 +1,22 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-@Client.on_message(filters.photo & filters.group & filters.bot & (filters.user(1733263647) | filters.user(1964681186)))
-async def reverse_handler(app: Client, message: Message):
+@Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1733263647))
+def reverse_handler(app: Client, message: Message):
     
     text = message.caption.split(' ')
 
-    if text[1] != "waifu" and text[1] != "husbando":
+    if text[1] != "waifu":
         return
     
-    return await message.reply_text("wa")
+    return message.reply_text("wa")
+
+@Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1964681186))
+def reverse_handler_husband(app: Client, message: Message):
+    
+    text = message.caption.split(' ')
+
+    if text[1] != "husbando":
+        return
+    
+    return message.reply_text("wa")
