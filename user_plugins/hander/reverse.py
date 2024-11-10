6cc628308a3acf07db865f3237bdd80bc5f10ecc
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-11-10 11:05:13 -0500

    Bye reverse detect

--- user_plugins/hander/reverse.py
+++ user_plugins/hander/reverse.py
@@ -1,22 +1,22 @@
-from pyrogram import Client, filters
-from pyrogram.types import Message
+# from pyrogram import Client, filters
+# from pyrogram.types import Message
 
-@Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1733263647))
-def reverse_handler(app: Client, message: Message):
+# @Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1733263647))
+# def reverse_handler(app: Client, message: Message):
     
-    text = message.caption.split(' ')
+#     text = message.caption.split(' ')
 
-    if text[1] != "waifu":
-        return
+#     if text[1] != "waifu":
+#         return
     
-    return message.reply_text("wa")
+#     return message.reply_text("wa")
 
-@Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1964681186))
-def reverse_handler_husband(app: Client, message: Message):
+# @Client.on_message(filters.photo & filters.group & filters.bot & filters.user(1964681186))
+# def reverse_handler_husband(app: Client, message: Message):
     
-    text = message.caption.split(' ')
+#     text = message.caption.split(' ')
 
-    if text[1] != "husbando":
-        return
+#     if text[1] != "husbando":
+#         return
     
-    return message.reply_text("wa")
+#     return message.reply_text("wa")
