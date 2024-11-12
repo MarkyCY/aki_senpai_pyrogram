040f1021e2e32e99bdb1297f9e2672679dd2c130
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2024-11-12 01:01:13 -0500

    Fix mongo slow

--- database/mongodb.py
+++ database/mongodb.py
@@ -1,7 +1,11 @@
 import os
 import motor.motor_asyncio
 
+from dotenv import load_dotenv
+load_dotenv()
+
+mongo_uri = os.getenv('MONGO_URI')
+client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
+
 async def get_db():
-    mongo_uri = os.getenv('MONGO_URI')
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongo_uri)
     return client.otakusenpai
