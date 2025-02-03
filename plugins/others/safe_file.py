52b3a94bcfa530dfaf65a2827b7d4d6dec1a55d5
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-02-03 18:16:08 -0500

    Show safety

--- plugins/others/safe_file.py
+++ plugins/others/safe_file.py
@@ -200,6 +200,7 @@ def check_content_safety(image_description, api_key):
             model="llama-guard-3-8b",
         )
         res = chat_completion.choices[0].message.content
+        print(res.lower())
         if "unsafe" in res.lower():
             return "unsafe"
         return "safe"
