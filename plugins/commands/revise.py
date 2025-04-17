3599ad86ae0322b4d5d34d9450c4974e4db7e142
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-04-17 18:56:25 -0400

    Change detection to NFSW

--- plugins/commands/revise.py
+++ plugins/commands/revise.py
@@ -1,9 +1,12 @@
 from pyrogram import Client, filters, utils, enums
 from pyrogram.types import Message, ChatPermissions
 
-from plugins.others.safe_file import process_image
+# from plugins.others.safe_file import process_image
 # from plugins.others.compare_img import compare_images
 from dotenv import load_dotenv
+from PIL import Image
+from user_plugins.funcs.group_stats import async_post_image
+import requests
 
 load_dotenv()
 
@@ -12,6 +15,7 @@ import base64
 import io
 
 groq_api = os.getenv('GROQ_API')
+imgbb_api = os.getenv('IMGBB_API')
 
 async def progress(current, total):
     print(f"{current * 100 / total:.1f}%")
@@ -45,8 +49,6 @@ async def revise_command(app: Client, message: Message):
     chat_id = message.chat.id
     group_perm = [-1001485529816, -1001664356911]
 
-    ban = False
-
     if not message.reply_to_message:
         return
     
@@ -72,36 +74,64 @@ async def revise_command(app: Client, message: Message):
         os.remove(downloaded_file)
         return await message.reply_text(text=f"Esto solo funciona con imagenes o stickers.")
     
+    with Image.open(downloaded_file) as img:
+        png_path = downloaded_file.rsplit(".", 1)[0] + ".png"
+        img.save(png_path, format="PNG")
+        os.remove(downloaded_file)
+        downloaded_file = png_path
 
-    # safe, explain = detect_safe_search(downloaded_file)
-
-    # resul_comp = await compare_images(downloaded_file)
-    prompt = "Describe all elements in this image in detail, including text, objects, and context. Say if the content is apropiated for kids."
-    
-    description, safety = process_image(downloaded_file, None, prompt, groq_api)
-    print(description, safety)
-
+    url = "https://api.imgbb.com/1/upload"
+    params = {
+        "key": imgbb_api,
+        "expiration": 60
+    }
+    response = await async_post_image(url, params, downloaded_file)
     os.remove(downloaded_file)
 
-    if safety == "unsafe":
-        ban = True
+    img_url = response["data"]["url"]
 
-    # if safe is False:
-    #     ban = True
+    url = "https://nsfw-demo.sashido.io/api/image/classify"
+    try:
+        res = requests.get(f"https://nsfw-demo.sashido.io/api/image/classify?url={img_url}")
+    except Exception as e:
+        print(e)
+        return
+    
+    data = res.json()
 
-    if ban is True:
+    for item in data:
+        match item['className']:
+            case "Neutral":
+                neutral = int(item['probability'] * 100)
+            case "Drawing":
+                drawing = int(item['probability'] * 100)
+            case "Porn":
+                porn = int(item['probability'] * 100)
+            case "Hentai":
+                hentai = int(item['probability'] * 100)
+            case "Sexy":
+                sexy = int(item['probability'] * 100)
+            
+    # await message.reply(text=f"**Resultados de la revisión:**\n\n"
+    #     f"Neutral: {neutral}%\n"
+    #     f"Drawing: {drawing}%\n"
+    #     f"Porn: {porn}%\n"
+    #     f"Hentai: {hentai}%\n"
+    #     f"Sexy: {sexy}%\n")
 
+    if porn >= 90 or hentai >= 90:
+        description = f"""El contenido de este mensaje ha sido marcado como NSFW con un {porn}% de probabilidad de ser pornográfico y un {hentai}% de probabilidad de ser hentai."""
         until_date=utils.zero_datetime()
         try:
             await app.restrict_chat_member(chat_id, message.reply_to_message.from_user.id, permissions, until_date=until_date)
         except Exception as e:
             pass
 
-        await message.reply_text(f"El usuario {message.reply_to_message.from_user.first_name} ha sido muteado por no ser completamente seguro de su contenido.")
+        await message.reply_text(f"El usuario {message.reply_to_message.from_user.first_name} ha sido muteado por no ser completamente seguro de su contenido.\n<blockquote>{description}</blockquote>", parse_mode=enums.ParseMode.MARKDOWN,)
         await app.forward_messages(-1001664356911, message.chat.id, message.reply_to_message.id, message_thread_id=82096)
         await app.send_message(
             -1001664356911,
-            text=f"Contenido no deseado de <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a>\n<blockquote>{description[:400]}</blockquote>", 
+            text=f"Contenido no deseado de <a href='tg://user?id={message.reply_to_message.from_user.id}'>{message.reply_to_message.from_user.first_name}</a>\n<blockquote>{description}</blockquote>", 
             parse_mode=enums.ParseMode.MARKDOWN,
             message_thread_id=82096
             )
@@ -109,5 +139,5 @@ async def revise_command(app: Client, message: Message):
         await app.delete_messages(chat_id, message.reply_to_message.id)
 
     else:
-        await message.reply_text(f"Esta imagen está permitida\n <blockquote>{description[:400]}</blockquote>", parse_mode=enums.ParseMode.MARKDOWN,)
+        await message.reply_text(f"Esta imagen está permitida\n <blockquote>{description}</blockquote>", parse_mode=enums.ParseMode.MARKDOWN,)
     
