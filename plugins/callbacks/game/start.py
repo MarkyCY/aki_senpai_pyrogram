364458d11d0ecd7c1b0209a847513142b00afb2f
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-09-27 10:36:15 -0400

    test game

---
+++ plugins/callbacks/game/start.py
@@ -0,0 +1,20 @@
+from pyrogram import Client
+from pyrogram.types import CallbackQuery
+from pyrogram import filters
+
+# from database.mongodb import get_db
+from plugins.others.contest import *
+import jwt
+
+
+# Handler específico para el juego
+@Client.on_callback_query(filters.game_short_name("settle_mints_game"))
+def handle_game_short_name(app: Client, call: CallbackQuery):
+    user_id = str(call.from_user.id)
+    # Aquí generas el token (implementación equivalente a createToken)
+    token = createToken(user_id)
+    # Responde la callback con la URL personalizada
+    call.answer(url=f"https://proyectos-game-z2vizx-7c7e26-82-180-160-194.traefik.me/?token={token}")
+
+def createToken(user_id):
+    return jwt.encode({"user_id": user_id}, "secret", algorithm="HS256")
