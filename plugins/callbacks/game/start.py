1216e385ff43eb839e98a172617754c80f959c1a
Marcos J. Cardenas P. <oranmarcos@gmail.com>
2025-09-27 19:01:15 -0400

    Finish

--- plugins/callbacks/game/start.py
+++ plugins/callbacks/game/start.py
@@ -1,10 +1,10 @@
 from pyrogram import Client
-from pyrogram.types import CallbackQuery
+from pyrogram.types import CallbackQuery, Message
 from pyrogram import filters
+from pyrogram.raw.functions.messages import SetGameScore
 
 # from database.mongodb import get_db
 from plugins.others.contest import *
-import jwt
 
 async def awa(_, __, message):
     #if message.game_short_name == "akira_runner":
@@ -12,20 +12,16 @@ async def awa(_, __, message):
     
 awa_detect = filters.create(awa)
 
+@Client.on_callback_query()
+async def awa(app: Client, call: CallbackQuery):
+    print(f"Callback data: {call}")
 
 # Handler específico para el juego
 @Client.on_callback_query(awa_detect)
-def handle_game_short_name(app: Client, call: CallbackQuery):
+async def handle_game_short_name(app: Client, call: CallbackQuery):
     user_id = str(call.from_user.id)
     username = call.from_user.username
-    # Aquí generas el token (implementación equivalente a createToken)
-    token = createToken(user_id)
+    inline_message_id = call.inline_message_id
+    print(call.inline_message_id)
     # Responde la callback con la URL personalizada
-    call.answer(url=f"https://proyectos-game-z2vizx-7c7e26-82-180-160-194.traefik.me/?username={username}")
-
-def createToken(user_id):
-    token = jwt.encode({"user_id": user_id}, "secret", algorithm="HS256")
-    if isinstance(token, bytes):
-        # Convertir a string para compatibilidad
-        token = token.decode("utf-8")
-    return token
+    await call.answer(url=f"https://proyectos-game-z2vizx-7c7e26-82-180-160-194.traefik.me/?username={username}&inline_message_id={inline_message_id}&user_id={user_id}")
